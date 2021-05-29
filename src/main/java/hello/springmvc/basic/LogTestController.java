8d162c905ed413025d61667a525cbb8b1fc5b078
std-freejia <std-freejia@gmail.com>
2021-05-30 00:41:11 +0900

    로깅 - slf4j.Logger : 로그를 콘솔 출력 뿐 만 아니라, 파일 저장, 네트워크 전송도 가능하다. log.debug("debug log " + name); // 이렇게 문자열 결합 처럼 작성하지 말기! 왜냐하면, '+'연산이 발생하기 때문이다! (로그레벨 설정 관련 없이 무조건 연산이 발생함. 연산은 메모리와 CPU를 소모한다)

--- src/main/java/hello/springmvc/basic/LogTestController.java
+++ src/main/java/hello/springmvc/basic/LogTestController.java
@@ -1,16 +1,19 @@
 package hello.springmvc.basic;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // @RestController : 반환값으로 뷰를 주지 않음. 반환 값을 HTTP body에 바로 입력한다.
+// @Slf4j : 로그 선언을 자동으로 해준다
+@Slf4j
 @RestController
 public class LogTestController {
 
     // 로그 선언
-    private final Logger log = LoggerFactory.getLogger(getClass()); // 내 클래스를 지정해준다.
+    //private final Logger log = LoggerFactory.getLogger(getClass()); // 내 클래스를 지정해준다.
 
     @GetMapping("/log-test")
     public String logTest(){
@@ -22,7 +25,9 @@ public class LogTestController {
         // application.properties에 특정 레벨을 설정하지 않으면, 디폴트 레벨은 info.
 
         log.trace("trace log={}", name);
-        log.debug("debug log={}", name);
+        log.debug("debug log " + name); // 이렇게 쓰지 말기!
+        // 왜냐하면, '+'연산이 발생하기 때문이다! (로그레벨 설정 관련 없이 무조건 연산이 발생함. 연산은 메모리와 CPU를 소모한다)
+
         log.info("info log={}", name);
         log.warn("warn log={}", name);
         log.error("error log={}", name); // 로그 포맷 : {}안에 name이 출력된다.
